9f20980b1935582096d4c6df5cbd9ce8c1009206
Finnur Torfason <finnurtorfa@gmail.com>
2013-01-21 22:22:53 +0100

    Small fix for ExcelListOutput class

--- AflafrettirAPI/ExcelListOutput.py
+++ AflafrettirAPI/ExcelListOutput.py
@@ -24,16 +24,16 @@ class ExcelListOutput(object):
   
   def __init__(self, landingList, filename, date1, date2):
     self.landing = landingList
-    self.landingList = sorted(landingList['Landings'], key=lambda k: k['Catch'],
+    self.landingList = sorted(landingList['Landings'], key=lambda k: k['Catch US'],
         reverse=True)
     self.wb = xlwt.Workbook('utf-8')
     self.style0 = xlwt.easyxf('font: name Arial, color-index black',
         num_format_str='#,##0') 
     self.style1 = xlwt.easyxf('font: name Arial, color-index black, bold on',
         num_format_str='#,##0') 
-    self.item = ['Skipaskrárnúmer', 'Nafn', 'Fjöldi', 'Heildarafli', 
-        'Mesti afli', 'Höfn', 'Veiðarfæri']
-    self.key = ['ShipID', 'Name', 'Number', 'Catch', 'Most', 'name-key', 'Gear']
+    self.item = ['Skipaskrárnúmer', 'Nafn', 'Fjöldi', 'Heildarafli reiknað e. höfnum', 
+        'Heildarafli reiknað e. teg.', 'Mesti afli', 'Höfn', 'Veiðarfæri']
+    self.key = ['ShipID', 'Name', 'Number', 'Total S', 'Total US', 'Most S', 'Harbour', 'Gear']
     self.filename = filename
     self.date1 = date1
     self.date2 = date2
@@ -63,25 +63,44 @@ class ExcelListOutput(object):
 # Main body
 ###################################################
 if __name__ == '__main__': # If run on it's own
+
   url = {
-      'url':'http://www.fiskistofa.is/veidar/aflaupplysingar/landanir-eftir-hofnum/landanir.jsp?dagurFra=01.12.2012&hofn=1&dagurTil=11.12.2012&magn=Samantekt',
-      'url1':'http://www.fiskistofa.is/veidar/aflaupplysingar/landanir-eftir-hofnum/landanir.jsp?dagurFra=01.12.2012&hofn=149&dagurTil=11.12.2012&magn=Samantekt',
+      'url':'http://www.fiskistofa.is/veidar/aflaupplysingar/landanir-eftir-hofnum/landanir.jsp?dagurFra=01.12.2012&hofn=1&dagurTil=11.12.2012&magn=Sundurlidun',
+      'url1':'http://www.fiskistofa.is/veidar/aflaupplysingar/landanir-eftir-hofnum/landanir.jsp?dagurFra=01.12.2012&hofn=149&dagurTil=11.12.2012&magn=Sundurlidun',
       }
-  landingList = []
+      
+  url2 = {
+      u'Síld 30':'http://www.fiskistofa.is/veidar/aflaupplysingar/afliallartegundir/aflastodulisti_okvb.jsp?p_fteg=Þorskur+1&p_fra=01.12.2012&p_til=11.12.2012',
+      u'Loðna 31':'http://www.fiskistofa.is/veidar/aflaupplysingar/afliallartegundir/aflastodulisti_okvb.jsp?p_fteg=Ufsi+3&p_fra=01.12.2012&p_til=11.12.2012'
+      }
+
+  harbour_list = []
+  species_list = []
+  landing_list = []
   html = QueryURL(url)
+  html2 = QueryURL(url2)
+  h_keys = ['Date', 'ShipID', 'Name', 'Gear', 'Catch S', 'Harbour']
+  s_keys = ['Name', 'Category', 'Catch US', 'Species']
 
   for i in html:
-    table = ParseHTML(i)
+    table = ParseHTML(i, [2, 1], ['Date', 'ShipID', 'Name', 'Gear', 'Stuff', 'Catch S'],
+        range(0,6), 'Harbour')
     for j in table:
-      landingList.append(j)
-      #print j
-
-  lists = TotalCatch(landingList)
-  landingList = []
+      harbour_list.append(j)
+      print j
+  
+  for i in html2:
+    table = ParseHTML(i, [1, 2], ['ShipID', 'Name', 'Category', 'Catch US'],
+        range(0,4), 'Species')
+    for j in table:
+      print j
+      species_list.append(j)
+  
+  lists = TotalCatch(harbour_list, species_list, h_keys, s_keys)
   for i in lists:
-    landingList.append(i)
+    landing_list.append(i)
 
-  List = {'Group':'Heild', 'Landings':landingList}
+  List = {'Group':'Heild', 'Landings':landing_list}
 
   Excel = ExcelListOutput(List, 'Hey.xls', '01.01.01', '01.01.10')
   Excel.save_excel()
