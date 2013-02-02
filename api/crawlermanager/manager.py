10d85b37ecb6e0472288cd88b412f70cdbf03f6c
Finnur Torfason <finnurtorfa@gmail.com>
2013-02-02 16:27:26 +0100

    Small changes. Moved the urlmanager directory to crawlermanager directory. Improved documentation

--- api/crawlermanager/manager.py
+++ api/crawlermanager/manager.py
@@ -2,24 +2,23 @@
 # *-* encoding: utf-8 *-*
 
 import Queue
+
 from threading import Thread
 
 class WebCrawler(object):
   def __init__(self, **kwargs):
-    self.base_url = 'http://www.fiskistofa.is/veidar/aflaupplysingar/'
-    self.h_url = self.base_url + 'landanir-eftir-hofnum/'
-    self.s_url = self.base_url + 'afliallartegundir/'
+    self.h_url = kwargs['h_url']
+    self.s_url = kwargs['s_url']
     
     (self.harbours, self.species) = self.fetch_get_params()
 
     self.h_url += 'landanir.jsp'
     self.s_url += 'aflastodulisti_okvb.jsp'
-    self.h_dict = kwargs['harbour']
-    self.s_dict = kwargs['species']
 
   def get_html(self, url, **kwargs):
     """ Sends a GET request, returns :class: 'Response' object
 
+    :param self: instance attribute of :class: 'WebCrawler' object
     :param url: a URL for the new :class: 'Request' object
     :param **kwargs: optional parameters that 'request' takes
     """
@@ -31,6 +30,7 @@ class WebCrawler(object):
     """ Passes :params: 'url' and 'param' to :function: 'get_html', returns
     :dict: object
 
+    :param self: instance attribute of :class: 'WebCrawler' object
     :param url: a URL for the new :class: 'Request' object
     :param param: An arguments that :class: 'BeautifulSoup' takes
     """
@@ -50,6 +50,8 @@ class WebCrawler(object):
 
   def fetch_get_params(self):
     """ Passes :params: to :function: 'get_params', returns :tuple: object:
+    
+    :param self: instance attribute of :class: 'WebCrawler' object
     """
     new_params = (
         (self.h_url, {'name':'hofn'}),
@@ -63,7 +65,25 @@ class WebCrawler(object):
 
     return result
 
+  def set_get_params(self, h_params, s_params):
+    """ Sets the GET params for querying the website of Directorate of
+    Fisheries(DOF) in Iceland.
+
+    :param self: instance attribute of :class: 'WebCrawler' object
+    :param h_params: GET parameters for harbours
+    :param s_params: GET parameters for species
+    """
+    self.h_params = h_params
+    self.s_params = s_params
+
   def populate_queue(self, param_name, queue, harbour=True):
+    """ Adds items to a :class: 'Queue' object
+
+    :param self: instance attribute of :class: 'WebCrawler' object
+    :param param_name: the name of the variable GET parameter
+    :param queue: a :class: 'Queue' object that items are added to
+    :param harbour: a boolean value to distinguish between harbours and species
+    """
     if harbour:
       p_dict = self.h_dict
       p_url = self.h_url
@@ -73,11 +93,10 @@ class WebCrawler(object):
       p_url = self.s_url
       params = self.species
 
-
     for p in params:
       tmp_dict = p_dict.copy()
       tmp_dict.update({param_name:params[p]})
-      queue.put((self.get_html, p_url, p, tmp_dict))
+      queue.put((self.get_html, p_url, tmp_dict, p, harbour))
 
 class WebCrawlerThread(Thread):
   def __init__(self, crawler, param_name, queue):
@@ -91,22 +110,28 @@ class WebCrawlerThread(Thread):
 
   def run(self):
     while True:
-      chunk = self.queue.get()
-      resp = chunk[0](chunk[1], **chunk[3])
+      get_params_cb = self.queue.get()
+      resp = get_params_cb[0](get_params_cb[1], **get_params_cb[2])
+      print resp.url, resp
 
       self.queue.task_done()
 
 def main():
-  args = {
-      'harbour':{'dagurFra':'01.01.2013', 'dagurTil':'10.01.2013', 'magn':'Sundurlidun'}, 
-      'species':{'p_fra':'01.01.2013', 'p_til':'10.01.2013'}
-      }
+  base_url = 'http://www.fiskistofa.is/veidar/aflaupplysingar/'
+  h_url = base_url + 'landanir-eftir-hofnum/'
+  s_url = base_url + 'afliallartegundir/'
+  url_dict = {'h_url':h_url, 's_url':s_url}
+  url_params = (
+      {'dagurFra':'01.01.2013', 'dagurTil':'10.01.2013', 'magn':'Sundurlidun'}, 
+      {'p_fra':'01.01.2013', 'p_til':'10.01.2013'}
+      )
   h_queue = Queue.Queue()
   s_queue = Queue.Queue()
-  crawler = WebCrawler(**args)
+  crawler = WebCrawler(**url_dict)
+  crawler.set_get_params(*url_params)
   crawler.populate_queue('hofn', h_queue)
   crawler.populate_queue('p_fteg', s_queue, False)
-  
+
   h_thread = WebCrawlerThread(crawler, 'hofn', h_queue)
   s_thread = WebCrawlerThread(crawler, 'p_fteg', s_queue)
 
@@ -115,8 +140,6 @@ def main():
 
   while (not h_queue.empty()) and (not s_queue.empty()):
     pass
-
-  print "Done"
 
 if __name__ == '__main__':
   main()
