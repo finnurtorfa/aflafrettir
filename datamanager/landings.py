c6e7f8c2de8f767f24350abcaa7f5d03f8d98f48
Finnur Torfason <finnurtorfa@gmail.com>
2013-07-23 13:57:24 +0200

    Add the special __add__() function to the class Landings() so it is possible to perform addition on two Landings() objects, as long as they have the same shipNumber

--- datamanager/landings.py
+++ datamanager/landings.py
@@ -9,27 +9,27 @@ manager
 from suds.client import Client
 from suds.sax.date import Date
 
+keys = ['landingCatch', 'landingDate', 'landingHarbour', 'shipGrossTonnage',
+    'shipName', 'shipNumber']
+groups = {
+  'Uppsjávarskip':[u'Loðnunót', u'Síldarnót', u'Flotvarpa', u'Loðnuflotvarpa',
+    u'Síldar-/kolmunnaflotvarpa'], 
+  'Net':[u'Net', u'Skötuselsnet', u'Grásleppunet', ], 
+  'Humar':[u'Humarvarpa'],
+  'Lína':[u'Lína'], 
+  'Rækja':[u'Rækjuvarpa'], 
+  'Botnvörpungar':[u'Botnvarpa'],
+  'Dragnót':[u'Dragnót 135 mm'], 
+  'Handfæri':[u'Handfæri'],
+  'Ýmislegt':[u'Ýmis veiðarfæri', u'Þorskgildra', u'Krabbagildra',
+    u'Hörpudiskplógur/Scallop dr.', u'Sjóstöng', u'Ígulkeraplógur',
+    u'RMNT', u'KRAL']}
+ 
 class Landings(object):
   """ :class Landings: contains info about landings such as catch, total catch,
   shipID, name etc.
   """
-  keys = ['landingCatch', 'landingDate', 'landingHarbour', 'shipGrossTonnage',
-      'shipName', 'shipNumber']
-  groups = {
-    'Uppsjávarskip':[u'Loðnunót', u'Síldarnót', u'Flotvarpa', u'Loðnuflotvarpa',
-      u'Síldar-/kolmunnaflotvarpa'], 
-    'Net':[u'Net', u'Skötuselsnet', u'Grásleppunet', ], 
-    'Humar':[u'Humarvarpa'],
-    'Lína':[u'Lína'], 
-    'Rækja':[u'Rækjuvarpa'], 
-    'Botnvörpungar':[u'Botnvarpa'],
-    'Dragnót':[u'Dragnót 135 mm'], 
-    'Handfæri':[u'Handfæri'],
-    'Ýmislegt':[u'Ýmis veiðarfæri', u'Þorskgildra', u'Krabbagildra',
-      u'Hörpudiskplógur/Scallop dr.', u'Sjóstöng', u'Ígulkeraplógur',
-      u'RMNT', u'KRAL']
-    }
- 
+
   def __init__(self):
     """ Initializes the :class Landings:
     """
@@ -42,13 +42,14 @@ class Landings(object):
     self.shipNumber       = None
     self.equipment        = None
     self.group            = None
+    self.count            = 1
     
   def set_variable(self, data):
     """ Checks if attributes from the :class Client: 'getLandings' method
     matching the names of the strings in the 'keys' class variable exist
     and passes them to the :class Landings: instance variables.
     """
-    for key in Landings.keys:
+    for key in keys:
       val = getattr(data, key)
       setattr(self, key, val)
 
@@ -58,18 +59,57 @@ class Landings(object):
     """
     if self.landingCatch is not None:
       self.totalCatch = 0
+      catch = dict()
       for i in self.landingCatch:
         self.totalCatch += i['totalCatch']
+        catch[i['species']] = i['totalCatch']
         if self.equipment is None:
           self.equipment = i['equipment']
 
-      for k,v in Landings.groups.iteritems():
+      self.landingCatch = catch
+
+      for k,v in groups.iteritems():
         for e in v:
           if self.equipment in e:
             self.group = k
             return
       else:
         self.group = 'Ýmislegt'
+
+  def __add__(self, add):
+    """ Returns the sum of two :class Landings(): given that the landings were
+    performed by the same ship. 
+    """
+    if self.shipNumber != add.shipNumber:
+      raise ValueError('Can only perform addition on landings from the same' 
+                        + ' ship')
+
+    self.totalCatch +=  add.totalCatch
+    
+    for k,v in add.landingCatch.iteritems():
+      if k in self.landingCatch:
+        self.landingCatch[k] += v
+      else:
+        self.landingCatch[k] = v
+
+    if type(self.landingDate) == type(list()):
+      self.landingDate.append(add.landingDate)
+    else:
+      self.landingDate = [self.landingDate, add.landingDate]
+    
+    if type(self.landingHarbour) == type(list()):
+      self.landingHarbour.append(add.landingHarbour)
+    else:
+      self.landingHarbour = [self.landingHarbour, add.landingHarbour]
+      
+    if type(self.equipment) == type(list()):
+      self.equipment.append(add.equipment)
+    else:
+      self.equipment = [self.equipment, add.equipment]
+    
+    self.count = len(self.landingDate)
+
+    return self
 
 if __name__ == '__main__':
   manager = SoapManager()
