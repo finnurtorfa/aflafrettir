73f79c2cd75046cbf1775e13d95ba6e677df66c2
Finnur Torfason <finnurtorfa@gmail.com>
2015-05-15 15:50:06 +0200

    Modularize the extension

---
+++ flask_aflafrettir/__init__.py
@@ -0,0 +1,17 @@
+"""
+  flask.aflafrettir
+  ~~~~~~~~~~~~~~
+
+  Adds support for fetching data from the Department of Fisheries in Iceland and
+  calculating the total catch for certain period of time.
+"""
+
+__author__      = u'Finnur Smári Torfason'
+__copyright__   = 'Copyright 2015, www.aflafrettir.com'
+__credits__     = [u'Finnur Smári Torfason', u'Gísli Reynisson']
+
+__license__     = 'GPL v3'
+__version__     = '0.2'
+__maintainer__  = u'Finnur Smári Torfason'
+__email__       = 'finnurtorfa@gmail.com'
+__status__      = 'Development'
