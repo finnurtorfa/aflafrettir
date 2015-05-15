8b7ae585dbbb301f26715d23bf741ba54f19fa92
Finnur Torfason <finnurtorfa@gmail.com>
2015-05-15 15:17:11 +0200

    Add a class for managing DOF SOAP service
    Add a class 'DOFManager' for managing the Icelandic Department of Fisheries's
    SOAP service.

--- flask_aflafrettir/soap/__init__.py
+++ flask_aflafrettir/soap/__init__.py
@@ -38,3 +38,81 @@ class SOAPManager(object):
     """
     return getattr(self.client.service, method)(*args)
   
+
+class DOFManager(SOAPManager):
+  """ A 'DOFManager' object that manages connection to the Department of
+  Fisheries in Iceland's SOAP service
+  """
+  def __init__(self, credentials, **kwargs):
+    """ Initialize the :class DOFManager: object.
+
+    :param credentials:   A dictionary containing username and password to the
+                          DOF's SOAP service.
+    :param kwargs:        A list of other keyword arguments to pass to DOF's
+                          SOAP service.
+    """
+    self.url = 'https://soap.fiskistofa.is/landing/v1/LandingService?wsdl'
+    super().__init__(url=self.url, headers=credentials, **kwargs)
+
+  def get_api_version(self):
+    """ Fetch the API version of the service.
+    """
+    return self.call_method('getAPIVersion')
+
+  def get_fishing_areas(self):
+    """ Fetch the registered fishing areas.
+    """
+    return self.call_method('getFishingAreas')
+
+  def get_fishing_equipment(self):
+    """ Fetch t
+    """
+    return self.call_method('getFishingEquipment')
+
+  def get_fishing_stocks(self):
+    """ Fetch the registered fishing stocks.
+    """
+    return self.call_method('getFishingStocks')
+
+  def get_species(self):
+    """ Fetch the registered fish species.
+    """
+    return self.call_method('getSpecies')
+
+  def get_states(self):
+    """ Fetch the registered states.
+    """
+    return self.call_method('getStates')
+
+  def get_storage_methods(self):
+    """ Fetch the registered states.
+    """
+    return self.call_method('getStorageMethods')
+
+  def get_all_landings(self, date_from, date_to):
+    """ Fetch all landings for a period of time. It is assumed that 
+    :param date_from" is less than or equal to :param date_to: or it will result
+    in an empty query.
+    
+    :param date_from:   A string|datetime|date object containing the start date
+    :param date_to:     A string|datetime|date object containing the end date
+    """
+    date_from = Date(date_from)
+    date_to = Date(date_to)
+
+    return self.call_method('getAllLandings', date_from, date_to)
+
+  def get_landings(self, date_from, date_to, ship_id):
+    """ Fetch all landings for a specific boat for a period of time. It is 
+    assumed that:param date_from" is less than or equal to :param date_to: or it
+    will result in an empty query.
+
+    :param date_from:   A string|datetime|date object containing the start date
+    :param date_to:     A string|datetime|date object containing the end date
+    :param ship_id:     A string with the ship's id
+    """
+    date_from = Date(date_from)
+    date_to = Date(date_to)
+
+    return self.call_method('getLandings', date_from, date_to, ship_id)
+
