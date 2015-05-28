9a34949888787daf62fe7275f173aea3e9eaebcf
Finnur Torfason <finnurtorfa@gmail.com>
2015-05-28 20:00:36 +0200

    Remove some stuff
    Remove GUI related stuff, py2exe related stuff and utils

--- setup/create_exe.bat
+++
@@ -1,4 +0,0 @@
-del dist
-C:\Python27\python.exe exe_setup.py py2exe
-xcopy /H ..\.config dist
-mkdir .\dist\log\
--- setup/exe_setup.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('..')
-from distutils.core import setup
-import py2exe
-
-setup(windows=['../aflafrettir.py'])
